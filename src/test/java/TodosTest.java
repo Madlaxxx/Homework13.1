4883d645d1a3a2b5227474f6d85da71c0802d36b
Мария <masha2355@gmail.com>
2023-12-30 16:44:04 +1000

    NewTodosTest

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -29,7 +29,7 @@ public class TodosTest {
     }
 
     @Test
-    public void searchTask() {
+    public void searchOneTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
@@ -50,6 +50,56 @@ public class TodosTest {
 
         Task[] expected = {epic};
         Task[] actual = todos.search("Яйца");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searchTwoTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Доделать приложение НетоБанка");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = todos.search("НетоБанк");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searchZeroTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Доделать приложение НетоБанка");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Искупать кота");
         Assertions.assertArrayEquals(expected, actual);
     }
 }
